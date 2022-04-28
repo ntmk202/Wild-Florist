f0f8b3d85862aa4f6c128e718a0b314a756580ac
nguyen minh khue <ntmkhue.20it6@vku.udn.vn>
2022-04-28 23:49:55 +0700

    initial commit

--- app/src/main/java/com/ntmk/myapp/model/User.kt
+++ app/src/main/java/com/ntmk/myapp/model/User.kt
@@ -1,10 +1,7 @@
 package com.ntmk.myapp.model
 
-import android.text.TextUtils
-import android.util.Patterns
-import java.util.regex.Pattern
 
-class User(
+data class User(
     val id:Int,
     val name: String,
     val email:String,
